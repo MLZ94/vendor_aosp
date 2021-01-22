ba180482da8bac1a2fb07e23708d22248fedf3e4
nahnah <nandanrmenon@gmail.com>
2021-01-22 09:35:13 +0530

    vendor: Set redfin build fingerprint

--- config/fingerprint.mk
+++ config/fingerprint.mk
@@ -1,2 +1,2 @@
 # Safetynet
-TARGET_FORCE_BUILD_FINGERPRINT := google/sunfish/sunfish:11/RP1A.201105.002/6869500:user/release-keys
+TARGET_FORCE_BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys
